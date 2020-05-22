86d5e2658cea324b2d3d426ec8c6b1fa4f2f5b32
Irina Belousova <irinabelousova11@gmail.com>
2020-05-22 23:48:26 +0400

    JS code was cleaned; README file was added.

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Panels</h1>
+<p>Panels were created for prqctice moving a lot of panels. I used data-* attribute in html elements for coordinates for moving. It helped me to reduce amount of JS code. I also practiced transform property for changing text in panels.</p>
+
+<h3>Technologies used:</h3>
+<ul>
+<li>HTML5</li>
+<li>CSS3</li>
+<li>JavaScript</li>
+</ul>
--- script.js
+++ script.js
@@ -1,83 +1,53 @@
 class Panels {
   constructor() {
+    this.siteWrap = document.getElementById("site-wrap");
+    this.mainName = document
+      .getElementById("main")
+      .querySelector("span[name='main']");
     this.mainButtons = document.getElementById("main").querySelectorAll("a");
+    this.upLeftButtons = document
+      .getElementById("upLeft")
+      .querySelectorAll("a");
+    this.upButtons = document.getElementById("up").querySelectorAll("a");
+    this.upRightButtons = document
+      .getElementById("upRight")
+      .querySelectorAll("a");
+    this.leftButtons = document.getElementById("left").querySelectorAll("a");
+    this.rightButtons = document.getElementById("right").querySelectorAll("a");
+    this.downLeftButtons = document
+      .getElementById("downLeft")
+      .querySelectorAll("a");
+    this.downButtons = document.getElementById("down").querySelectorAll("a");
+    this.downRightButtons = document
+      .getElementById("downRight")
+      .querySelectorAll("a");
 
     this.addListeners();
   }
 
-  addListeners() {
-    for (let element in this) {
-      for (let item of this[element]) {
-        console.log(item);
-        item.addEventListener("click", this.changePosition);
-      }
-    }
+  changePosition(top, left) {
+    this.siteWrap.style.top = top;
+    this.siteWrap.style.left = left;
   }
 
-  changePosition() {
-    siteWrap.style.top = this.getAttribute("data-top");
-    siteWrap.style.left = this.getAttribute("data-left");
+  addListeners() {
+    this.mainName.addEventListener("click", () => {
+      this.siteWrap.classList.toggle("active");
+    });
+
+    for (let element in this) {
+      if ((element !== "siteWrap") & (element !== "mainName")) {
+        for (let item of this[element]) {
+          item.addEventListener("click", () => {
+            this.changePosition(
+              item.getAttribute("data-top"),
+              item.getAttribute("data-left")
+            );
+          });
+        }
+      }
+    }
   }
 }
 
 let panels = new Panels();
-// console.log(panels.mainButtons);
-
-const siteWrap = document.getElementById("site-wrap");
-const mainPanel = document.getElementById("main");
-const upPanel = document.getElementById("up");
-const upRightPanel = document.getElementById("upRight");
-
-const mainUpBtn = mainPanel.querySelector("[name='main-up']");
-const mainUpRightBtn = mainPanel.querySelector("[name='main-up-right']");
-
-const upDownBtn = upPanel.querySelector("[name='up-down']");
-const upRightBtn = upPanel.querySelector("[name='up-right']");
-
-const upRightDownLeftBtn = upRightPanel.querySelector(
-  "[name='upRight-down-left']"
-);
-const upRightLeftBtn = upRightPanel.querySelector("[name='upRight-left']");
-
-// console.log(mainUpBtn);
-
-// function changePosition() {
-//   siteWrap.style.top = this.getAttribute("data-top");
-//   siteWrap.style.left = this.getAttribute("data-left");
-// }
-
-// mainUpBtn.addEventListener("click", changePosition);
-// mainUpRightBtn.addEventListener("click", changePosition);
-
-upDownBtn.addEventListener("click", function () {
-  //   mainPanel.style.top = "0";
-  //   upPanel.style.top = "-100vh";
-  siteWrap.style.top = "-100vh";
-});
-
-upRightDownLeftBtn.addEventListener("click", function () {
-  //   mainPanel.style.top = "0";
-  //   mainPanel.style.left = "0";
-  //   upRightPanel.style.top = "-100vh";
-  //   upRightPanel.style.left = "100vw";
-  siteWrap.style.top = "-100vh";
-  siteWrap.style.left = "0";
-});
-
-upRightLeftBtn.addEventListener("click", function () {
-  //   upPanel.style.top = "0";
-  //   upPanel.style.left = "0";
-  //   upRightPanel.style.top = "-100vh";
-  //   upRightPanel.style.left = "100vw";
-  //   siteWrap.style.top = "-100vh";
-  siteWrap.style.left = "0";
-});
-
-upRightBtn.addEventListener("click", function () {
-  //   upPanel.style.top = "0";
-  //   upPanel.style.left = "0";
-  //   upRightPanel.style.top = "-100vh";
-  //   upRightPanel.style.left = "100vw";
-  //   siteWrap.style.top = "-100vh";
-  siteWrap.style.left = "-100vw";
-});
